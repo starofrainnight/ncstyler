978494ed06bc9f1dbad048a94c8bdaecba72c7d5
Hong-She Liang <starofrainnight@gmail.com>
2016-05-07 04:08:54 +0800

    Added command line arguments

--- src/ncstyler/console.py
+++ src/ncstyler/console.py
@@ -1,8 +1,16 @@
 
+import argparse
 
 def main():
-    pass
+    description='''A styler just target to naming conventions of source code'''
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("-o", "--output", help="Output file path")
+    parser.add_argument("file_path", help="Source file path")
+    
+    args = parser.parse_args()
+    print(args)
     
 if __name__ == "__main__":
-    # execute only if run as a script
+    # Execute only if run as a script
     main()
