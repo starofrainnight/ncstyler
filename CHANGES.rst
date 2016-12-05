92f8a24aa6e979951f27011407ce3eb3212f6d16
Hong-She Liang <starofrainnight@gmail.com>
2016-12-05 13:46:01 +0800

    Bump version to 0.1.7

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,3 +1,6 @@
 Changes
 =======
 
+0.1.7 (2016-12-05)
+
+* Fixed exception thrown when met nameless object
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ long_description=(
 
 setup(
     name=package_name,
-    version="0.1.6",
+    version="0.1.7",
     author="Hong-She Liang",
     author_email="starofrainnight@gmail.com",
     url="https://github.com/starofrainnight/%s" % package_name,
